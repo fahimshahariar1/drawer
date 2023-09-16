1583fe7a52dc675d11fb96c6a8876ae1d7902079
Fahim <fahimshahariar.fs@gmail.com>
2023-09-16 21:46:26 +0600

    third

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -11,14 +11,30 @@ class _DrawerDemoState extends State<DrawerDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar:  AppBar(),
-
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+      ),
       drawer: Drawer(
-        backgroundColor: Colors.deepPurple,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(100),
+            bottomRight: Radius.circular(100),
+          ),
+        ),
+        backgroundColor: Colors.pinkAccent,
         child: ListView(
           children: [
-            
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://media.licdn.com/dms/image/D5603AQEmR5j0Wb55eQ/profile-displayphoto-shrink_800_800/0/1668660177600?e=2147483647&v=beta&t=EB1ZbfwR5WRiwRacvzZOxo37SO4XcE3QURFG4HhLXsc"),
+                ),
+                accountName: Text("Fahim"),
+                accountEmail: Text("fahim@gmail.com"),
+              ),
+            ),
             ListTile(
               leading: Icon(Icons.home, color: Colors.white),
               title: Text("Home"),
@@ -38,7 +54,46 @@ class _DrawerDemoState extends State<DrawerDemo> {
           ],
         ),
       ),
-
+      endDrawer: Drawer(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(100),
+            bottomRight: Radius.circular(100),
+          ),
+        ),
+        backgroundColor: Colors.pinkAccent,
+        child: ListView(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://media.licdn.com/dms/image/D5603AQEmR5j0Wb55eQ/profile-displayphoto-shrink_800_800/0/1668660177600?e=2147483647&v=beta&t=EB1ZbfwR5WRiwRacvzZOxo37SO4XcE3QURFG4HhLXsc"),
+                ),
+                accountName: Text("Fahim"),
+                accountEmail: Text("fahim@gmail.com"),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home, color: Colors.white),
+              title: Text("Home"),
+            ),
+            ListTile(
+              leading: Icon(Icons.contact_phone),
+              title: Text("Contact"),
+            ),
+            ListTile(
+              leading: Icon(Icons.facebook),
+              title: Text("Facebook"),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Settings"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
